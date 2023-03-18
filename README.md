97d5f856994753dba93e65f0c759ac33a2486403
Viki <hi@viki.moe>
2023-03-18 21:01:37 +0800

    feat: up to 1.1.0 by GPT-4

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 自动添加空格 - 油猴脚本
 
-> 脚本由 [ChatGPT](https://chat.openai.com/chat) 编写并优化，可能存在 BUG。
+> 脚本由 [ChatGPT GPT-4 模型](https://chat.openai.com/chat) 编写，可能存在 BUG。
 
-使用 [pangu.js](https://github.com/vinta/pangu.js/blob/master/dist/browser/pangu.js) 在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、动态追加内容、动态更新 DOM 等情况。
+使用 [pangu.js](https://github.com/vinta/pangu.js/blob/master/dist/browser/pangu.js) 在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、DOM 动态更新等情况。
 
 ## 测试例子
 
--- index.js
+++ index.js
@@ -1,95 +1,69 @@
 // ==UserScript==
 // @name         自动添加空格，在 CJK （中日繁）字符和英文字母之间自动添加空格
 // @namespace    pangu-userscript
-// @version      1.0.3
+// @version      1.1.0
 // @license      MIT
-// @description  在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、动态追加内容、动态更新 DOM 等情况。
+// @description  在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、DOM 动态更新等情况。
 // @match        http*://*/*
 // @grant        none
 // ==/UserScript==
-;(() => {
-  window.addEventListener('load', () => {
-    const panguCdnLink = 'https://unpkg.com/pangu@4.0.7/dist/browser/pangu.min.js'
 
-    function loadScript(url) {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement('script')
-        script.src = url
-        script.onload = () => resolve()
-        script.onerror = () => reject(new Error(`Failed to load script ${url}`))
-        document.body.appendChild(script)
-      })
-    }
+;(function () {
+  'use strict'
 
-    async function init() {
-      try {
-        await loadScript(panguCdnLink)
-
-        // 处理当前 DOM 树中的节点
-        processTextNodes(document.body)
-
-        // 创建 MutationObserver 对象，监视 DOM 树的变化
-        const observer = new MutationObserver(mutationsList => {
-          // 遍历 MutationRecord 列表，查找新增的 DOM 节点
-          for (const mutation of mutationsList) {
-            for (const addedNode of mutation.addedNodes) {
-              // 如果新增的节点是元素节点，则处理它的子节点
-              if (addedNode.nodeType === Node.ELEMENT_NODE) {
-                processTextNodes(addedNode)
-              }
-            }
-          }
-        })
-
-        // 配置 MutationObserver，监视子树中的节点添加、删除、属性变化等情况
-        const config = { childList: true, subtree: true, attributes: true, attributeOldValue: true }
-        observer.observe(document.body, config)
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    if (/[^\u0000-\u00ff]/.test(document.body.innerText)) {
-      init()
-    }
-
-    function processTextNodes(rootNode) {
-      const walker = document.createTreeWalker(rootNode, NodeFilter.SHOW_TEXT, {
-        acceptNode: node =>
-          /[^\s]/.test(node.textContent) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT
-      })
-
-      let node
-      while ((node = walker.nextNode())) {
-        if (shouldSkipNode(node)) {
-          continue
-        }
-
-        const preBlank = /^\s*/.exec(node.textContent)[0]
-        const subBlank = /\s*$/.exec(node.textContent)[0]
-
-        const text = node.textContent.trim()
-
-        if (/[^\u0000-\u00ff]/.test(text)) {
-          node.textContent = preBlank + pangu.spacing(text) + subBlank
+  function addSpacing() {
+    const elements = document.querySelectorAll(
+      'body *:not(script):not(style):not(noscript):not(pre):not(code)'
+    )
+    elements.forEach(element => {
+      if (element.childNodes.length === 1 && element.childNodes[0].nodeType === Node.TEXT_NODE) {
+        const originalText = element.textContent
+        const newText = pangu.spacing(originalText)
+        if (originalText !== newText) {
+          element.textContent = newText
         }
       }
-    }
+    })
+  }
 
-    function shouldSkipNode(node) {
-      const parentTagName = node.parentNode.tagName
-      if (parentTagName === 'CODE' || parentTagName === 'PRE') {
-        return true
+  function observeDOM(observer) {
+    observer.observe(document.body, { childList: true, subtree: true })
+  }
+
+  function debounce(func, wait, immediate) {
+    let timeout
+    return function () {
+      const context = this,
+        args = arguments
+      const later = function () {
+        timeout = null
+        if (!immediate) func.apply(context, args)
       }
-
-      if (parentTagName === 'A') {
-        const href = node.parentNode.getAttribute('href')
-        if (href && href.startsWith('javascript:')) {
-          return true
-        }
-      }
-
-      return false
+      const callNow = immediate && !timeout
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+      if (callNow) func.apply(context, args)
     }
+  }
+
+  const addSpacingDebounced = debounce(addSpacing, 300)
+
+  // Run once when the script is loaded
+  addSpacing()
+
+  // Run every time the content of the page is changed
+  const observer = new MutationObserver(() => {
+    requestAnimationFrame(() => {
+      addSpacingDebounced()
+    })
   })
+
+  // Start observing when the DOM is ready
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+      observeDOM(observer)
+    })
+  } else {
+    observeDOM(observer)
+  }
 })()
