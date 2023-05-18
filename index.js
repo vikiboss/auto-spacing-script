3c7529ebb39a1329af0b7e621178d1fdd4de5f3c
Viki <hi@viki.moe>
2023-05-18 09:01:03 +0800

    fix: 修复 contenteditable 子元素不过滤问题
    使用 createTreeWalker 忽略所有 contenteditable 的子元素

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动添加空格，在 CJK （中日繁）字符和英文字母之间自动添加空格
 // @namespace    pangu-userscript
-// @version      1.2.1
+// @version      1.3.0
 // @license      MIT
 // @description  在 CJK （中日繁）字符和英文字母之间自动添加空格，考虑了代码块、DOM 动态更新等情况。
 // @match        http*://*/*
@@ -10,62 +10,83 @@
 // ==/UserScript==
 
 ;(function () {
-  'use strict';
+  'use strict'
+
+  const ignores = ['SCRIPT', 'STYLE', 'NOSCRIPT', 'PRE', 'CODE', 'INPUT']
+
+  const isIgnore = name => ignores.includes(name)
+  const isEditable = el => el.getAttribute('contenteditable')?.toLowerCase() === 'true'
+
+  function acceptNode(node) {
+    const isAccept = isIgnore(node.tagName) || isEditable(node)
+    return isAccept ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT
+  }
 
   function addSpacing() {
-    const elements = document.querySelectorAll(
-      'body *:not(script):not(style):not(noscript):not(pre):not(code):not(input):not([contenteditable="true"])'
-    );
+    const walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_ELEMENT,
+      { acceptNode },
+      false
+    )
+
+    const elements = []
+
+    while (walker.nextNode()) {
+      elements.push(walker.currentNode)
+    }
+
     elements.forEach(element => {
       if (element.childNodes.length === 1 && element.childNodes[0].nodeType === Node.TEXT_NODE) {
-        const originalText = element.textContent;
-        const newText = pangu.spacing(originalText);
+        const originalText = element.textContent
+        const newText = pangu.spacing(originalText)
+
         if (originalText !== newText) {
-          element.textContent = newText;
+          element.textContent = newText
         }
       }
-    });
+    })
   }
 
   function observeDOM(observer) {
-    observer.observe(document.body, { childList: true, subtree: true });
+    observer.observe(document.body, { childList: true, subtree: true })
   }
 
   function debounce(func, wait, immediate) {
-    let timeout;
+    let timeout
     return function () {
-      const context = this;
-      const args = arguments;
+      const context = this
+      const args = arguments
+
       const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
+        timeout = null
+        if (!immediate) func.apply(context, args)
+      }
+
+      const callNow = immediate && !timeout
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+
+      if (callNow) func.apply(context, args)
+    }
   }
 
-  const addSpacingDebounced = debounce(addSpacing, 300);
+  const addSpacingDebounced = debounce(addSpacing, 300)
 
   // Run once when the script is loaded
-  addSpacing();
+  addSpacing()
 
   // Run every time the content of the page is changed
   const observer = new MutationObserver(() => {
     requestAnimationFrame(() => {
-      addSpacingDebounced();
-    });
-  });
+      addSpacingDebounced()
+    })
+  })
 
   // Start observing when the DOM is ready
   if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', () => {
-      observeDOM(observer);
-    });
+    document.addEventListener('DOMContentLoaded', () => observeDOM(observer))
   } else {
-    observeDOM(observer);
+    observeDOM(observer)
   }
-})();
-
+})()
